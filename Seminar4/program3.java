184f2eceab5d75cb42a526426f3f2e9448d89a25
Петрашова Татьяна <121371007+PetrashTank@users.noreply.github.com>
2023-05-04 13:16:20 +0800

    Решение задачи 3 семинара 4 *

---
+++ Seminar4/program3.java
@@ -0,0 +1,57 @@
+package Seminar4;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+public class program3 {
+
+    private static boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static void calculator(String str) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        String[] newStr = str.split(" ");
+        System.out.println(Arrays.toString(newStr));
+        int result = 0;
+        for (int i = 0; i < newStr.length; i++) {
+            if (isDigit(newStr[i])) {
+                stack.push(Integer.parseInt(newStr[i]));
+            } else {
+                switch (newStr[i]) {
+                    case "+":
+                        result = stack.pop() + stack.pop();
+                        stack.push(result);
+                        break;
+                    case "-":
+                        result = stack.pop() - stack.pop();
+                        stack.push(result);
+                        break;
+                    case "*":
+                        result = stack.pop() * stack.pop();
+                        stack.push(result);
+                        break;
+                    case "/":
+                        result = stack.pop() / stack.pop();
+                        stack.push(result);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        System.out.println(stack);
+    }
+
+    public static void main(String[] args) {
+        String str = "5 7 6 * 1 * + 5 4 *";
+
+        calculator(str);
+    }
+
+}
